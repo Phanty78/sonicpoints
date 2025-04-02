0331af2c3fa771e0075703ebdbe5f98fda4b6c1f
Mael Donnart <maeldonnart@hotmail.fr>
2025-04-02 17:28:17 +0200

    Refactor project configuration and improve code consistency
    - Updated ESLint and PostCSS configuration files for consistent quotation style.
    - Removed package-lock.json to streamline dependency management.
    - Enhanced pnpm-lock.yaml with improved formatting for better readability.
    - Adjusted layout.js and globals.css for consistent import syntax.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,14 @@
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: 'SonicPoints',
   description: 'All your airdrop points in one place',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
