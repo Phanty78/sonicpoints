401630d46c6a37f5697e6363be090b50576c54de
Mael Donnart <maeldonnart@hotmail.fr>
2025-04-06 11:24:11 +0200

    Enhance data handling in Home component and SwapX API route
    - Added a new useEffect in the Home component to log swapxData and gemxAmount for better debugging and monitoring.
    - Updated the SwapX API route to include error handling for missing Sonic API key and GemX address, improving robustness.
    - Refactored the data fetching logic to use a constructed RequestURL, enhancing clarity and maintainability of the API call.
    - Ensured that console logs provide useful information for developers during the data fetching process.

--- src/app/api/swapx/route.js
+++ src/app/api/swapx/route.js
@@ -1,10 +1,19 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request) {
-  const GemXAddress = '0x05F0c7Ca7B90e3786603108D42cA8DFd28d72075';
   try {
     const { searchParams } = new URL(request.url);
     const address = searchParams.get('address');
+    const sonicApiKey = process.env.NEXT_PUBLIC_SONIC_API_KEY;
+    const GemXAddress = '0x05F0c7Ca7B90e3786603108D42cA8DFd28d72075';
+    const RequestURL = `https://api.sonicscan.org/api
+        ?module=account
+        &action=tokenbalance
+        &contractaddress=${GemXAddress}
+        &address=${address}
+        &tag=latest&apikey=${sonicApiKey}`;
+
+    console.log(RequestURL);
 
     if (!address) {
       return NextResponse.json(
@@ -13,16 +22,23 @@ export async function GET(request) {
       );
     }
 
-    const response = await fetch(
-      `https://api.sonicscan.org/api
-        ?module=account
-        &action=tokenbalance
-        &contractaddress=${GemXAddress}
-        &address=${address}
-        &tag=latest&apikey=${process.env.NEXT_PUBLIC_SONIC_API_KEY}`
-    );
-    const data = await response.json();
+    if (!sonicApiKey) {
+      return NextResponse.json(
+        { error: 'Sonic API key is not set' },
+        { status: 500 }
+      );
+    }
 
+    if (!GemXAddress) {
+      return NextResponse.json(
+        { error: 'GemX address is not set' },
+        { status: 500 }
+      );
+    }
+
+    const response = await fetch(RequestURL);
+    const data = await response.json();
+    console.log(data);
     return NextResponse.json(data);
   } catch (error) {
     console.error('Error in sonic route:', error);
